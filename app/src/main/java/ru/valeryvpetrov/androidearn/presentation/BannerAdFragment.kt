e0d6e5a07d627c2747f36b112fb73e41fdc27671
Valery Petrov <valeryvpetrov.dev@gmail.com>
2020-02-17 16:39:26 +0300

    Setup ad banner

--- app/src/main/java/ru/valeryvpetrov/androidearn/presentation/BannerAdFragment.kt
+++ app/src/main/java/ru/valeryvpetrov/androidearn/presentation/BannerAdFragment.kt
@@ -1,13 +1,27 @@
 package ru.valeryvpetrov.androidearn.presentation
 
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import kotlinx.android.synthetic.main.fragment_banner_ad.*
 import ru.valeryvpetrov.androidearn.R
 import ru.valeryvpetrov.androidearn.common.BaseFragment
+import ru.valeryvpetrov.androidearn.logger.logD
 
 class BannerAdFragment : BaseFragment() {
 
     override val layoutId = R.layout.fragment_banner_ad
 
     override fun setupViews() {
-         // TODO("not implemented")
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+        adView.adListener = object : AdListener() {
+            // Handle add related events
+            // For more information see: https://developers.google.com/android/reference/com/google/android/gms/ads/AdListener
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                logD("Add is loaded: $this")
+            }
+        }
     }
 }
