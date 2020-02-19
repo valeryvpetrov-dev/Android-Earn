265aa36bda6b9de4638f33894cda7bd2ede66e62
Valery Petrov <valeryvpetrov.dev@gmail.com>
2020-02-19 10:44:27 +0300

    Setup adaptive banner ad

--- app/src/main/java/ru/valeryvpetrov/androidearn/presentation/BannerAdFragment.kt
+++ app/src/main/java/ru/valeryvpetrov/androidearn/presentation/BannerAdFragment.kt
@@ -1,7 +1,11 @@
 package ru.valeryvpetrov.androidearn.presentation
 
+import android.util.DisplayMetrics
+import android.view.Display
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import kotlinx.android.synthetic.main.fragment_banner_ad.*
 import ru.valeryvpetrov.androidearn.R
 import ru.valeryvpetrov.androidearn.common.BaseFragment
@@ -9,10 +13,14 @@ import ru.valeryvpetrov.androidearn.logger.logD
 
 class BannerAdFragment : BaseFragment() {
 
+    private var initialLayoutComplete = false
+    private lateinit var adaptiveBannerAdView: AdView
+
     override val layoutId = R.layout.fragment_banner_ad
 
     override fun setupViews() {
         loadStandardBanner()
+        setupAdaptiveBanner()
     }
 
     private fun loadStandardBanner() {
@@ -27,5 +35,39 @@ class BannerAdFragment : BaseFragment() {
                 logD("Add is loaded: $this")
             }
         }
+    }
+
+    private fun setupAdaptiveBanner() {
+        adaptiveBannerAdView = AdView(requireContext())
+        adaptiveBannerAdView.adUnitId = getString(R.string.test_banner_ad_unit_id)
+        bannerAdaptiveContainer.addView(adaptiveBannerAdView)
+        bannerAdaptiveContainer.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete) {
+                initialLayoutComplete = true
+                loadAdaptiveBanner()
+            }
+        }
+    }
+
+    private fun loadAdaptiveBanner() {
+        val adRequest = AdRequest.Builder().build()
+        val adSize = calculateAdSize()
+        adaptiveBannerAdView.adSize = adSize
+        adaptiveBannerAdView.loadAd(adRequest)
+    }
+
+    private fun calculateAdSize(): AdSize {
+        val display: Display = requireActivity().windowManager.defaultDisplay
+        val outMetrics = DisplayMetrics()
+        display.getMetrics(outMetrics)
+
+        val density = outMetrics.density
+        var widthPixels = bannerAdaptiveContainer.width.toFloat()
+        if (widthPixels == 0f) {
+            widthPixels = outMetrics.widthPixels.toFloat()
+        }
+
+        val adWidth = (widthPixels / density).toInt()
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(requireContext(), adWidth)
     }
 }
